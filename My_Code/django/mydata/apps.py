d777fc6dd99f53586c5eae84c6ca22bbd44e6957
MTreker <150871931+MTreker@users.noreply.github.com>
2025-07-11 16:05:16 -0500

    Delete My_Code directory

--- My_Code/django/mydata/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MydataConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mydata'
--- My_Code/django/mydata/models.py
+++
@@ -1,6 +0,0 @@
-# models.py
-from django.db import models
-
-class Item(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
